2d6f3753ccea53dc2071283158261f4fb9f84094
ArtArcanjo <arthurarcanjo14@gmail.com>
2023-10-22 22:30:08 -0300

    Terceiro commit

--- src/pages/home/detailRaces.tsx
+++ src/pages/home/detailRaces.tsx
@@ -6,16 +6,17 @@ interface DetailRacesProps {
     arthur: string;
 }
 
-export default function detailRaces({arthur}: DetailRacesProps) : JSX.Element {
+export default function DetailRaces({ arthur }: DetailRacesProps): JSX.Element {
     const navigate = useNavigate();
 
     const handleButtonClick = () => {
-      
-      navigate('/play');
+        navigate('/play');
     }
 
-return <Container sx={{display:'flex',width:'55vw', height:'70vh', backgroundImage: 'url(BGspace.jpg)', backgroundSize: 'cover'}}>
-<Box component="img" src={arthur} sx={{width:"150px", height:"150px"}}/>
-<Button onClick={handleButtonClick} startIcon={<img src="play.png" alt="Play" width="35" height="35" />} sx={{position: 'absolute', bottom: '20px', right: '20px',}}/>
-</Container>
+    return (
+        <Container sx={{ display: 'flex', width: '55vw', height: '70vh', backgroundImage: 'url(BGspace.jpg)', backgroundSize: 'cover' }}>
+            <Box component="img" src={arthur} sx={{ width: "150px", height: "150px" }} />
+            <Button onClick={handleButtonClick} startIcon={<img src="play.png" alt="Play" width="35" height="35" />} sx={{ position: 'absolute', bottom: '20px', right: '20px' }} />
+        </Container>
+    );
 }
--- src/pages/play/play.tsx
+++ src/pages/play/play.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import { Button, Box, Container } from '@mui/material';
 
 export default function Play() {
     
-    return <p>teste</p>
+    return <Container sx={{backgroundColor:'black', position:'fixed', width:'100%', height:'100%', border:"0px"}}>
+
+
+    </Container>
 
 }
