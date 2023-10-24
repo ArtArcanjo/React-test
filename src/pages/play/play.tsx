897fa2031e9ec2829208d9af36e2cf3edb76bcc3
ArtArcanjo <arthurarcanjo14@gmail.com>
2023-10-24 16:14:13 -0300

    Quarto commit

--- src/pages/play/play.tsx
+++ src/pages/play/play.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 import { Button, Box, Container } from '@mui/material';
 
+
 export default function Play() {
-    
-    return <Container sx={{backgroundColor:'black', position:'fixed', width:'100%', height:'100%', border:"0px"}}>
+    const navigate = useNavigate();
+
+    const handleStartClick = () => {
+        
+    }
+    const handleProfileClick = () => {
+        
+    }
+    const handleExitClick = () => {
+        navigate('/');
+    }
 
 
-    </Container>
+    return <Box sx={{backgroundColor:'black', position:'fixed', width:'100%', height:'100%', border:"0px", display:'flex', justifyContent: 'center', flexDirection:'column', alignItems:"center"}}>
+    <Box component='img' src="title.png" sx={{display:'flex', width:'25vw', height:'40vh'}}>
+    </Box>
+    <Button variant="outlined" sx={{height:"40px", width:"200px", margin:"30px", color:"gray", borderColor:"gray"}}>Inicio</Button>
+    <Button variant="outlined" sx={{height:"40px", width:"200px", margin:"30px", color:"gray", borderColor:"gray"}}>Perfil</Button>
+    <Button onClick={handleExitClick} variant="outlined" sx={{height:"40px", width:"200px", margin:"30px", color:"darkred", borderColor:"gray"}}>Sair</Button>
+    </Box>
 
 }
